d2ff70be8a53a298388a897c3240c511feb0ac7b
pos <pgousdal@users.noreply.github.com>
2026-09-08 01:46:27 +0200

    M5.6: set receipt content type before status

--- submit/cmd/amiguard-submit/main.go
+++ submit/cmd/amiguard-submit/main.go
@@ -231,13 +231,14 @@ func handleSubmission(w http.ResponseWriter, r *http.Request, cfg config) {
 	_ = syncDir(cfg.quarantineRoot)
 
 	w.Header().Set("Cache-Control", "no-store")
-	w.WriteHeader(http.StatusCreated)
 	if wantsHTMLReceipt(r) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusCreated)
 		_, _ = fmt.Fprint(w, receiptPage(receipt))
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 	_ = json.NewEncoder(w).Encode(receipt)
 }
 
